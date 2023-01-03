d00f87a68ce914dd868e2948e82f9629a27e8e14
Pedro Furlan <pedrofurlan6@gmail.com>
2023-01-03 20:20:39 -0300

    Status Component

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,3 +1,4 @@
+import { Status } from '../History/styles'
 import { HistoryContainer, HistoryList } from './styles'
 
 export function History() {
@@ -20,31 +21,25 @@ export function History() {
               <td>Tarefa</td>
               <td>20 minutos</td>
               <td>Há 2 meses</td>
-              <td>Concluído</td>
+              <Status statusColor="green">Concluído</Status>
             </tr>
             <tr>
               <td>Tarefa</td>
               <td>20 minutos</td>
               <td>Há 2 meses</td>
-              <td>Concluído</td>
+              <Status statusColor="green">Concluído</Status>
             </tr>
             <tr>
               <td>Tarefa</td>
               <td>20 minutos</td>
               <td>Há 2 meses</td>
-              <td>Concluído</td>
+              <Status statusColor="yellow">Em andamento</Status>
             </tr>
             <tr>
               <td>Tarefa</td>
               <td>20 minutos</td>
               <td>Há 2 meses</td>
-              <td>Concluído</td>
-            </tr>
-            <tr>
-              <td>Tarefa</td>
-              <td>20 minutos</td>
-              <td>Há 2 meses</td>
-              <td>Concluído</td>
+              <Status statusColor="red">Interrompido</Status>
             </tr>
           </tbody>
         </table>
